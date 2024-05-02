6bb179e8e6618d40a6fcb0c1412be47076e5fe13
nikolay <n.ilin@it-park.tech>
2024-05-02 16:26:18 +0300

    hw2

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
--- SQL-команды для создания таблиц
+create table customers (
+  customer_id varchar(50) primary key not null,
+  company_name varchar(100),
+  contact_name varchar(100)
+);
+
+create table employee (
+  employee_id integer primary key not null,
+  first_name varchar(50),
+  last_name varchar(50),
+  title varchar(100),
+  birth_date date,
+  notes text
+);
+
+create table orders (
+  order_id integer primary key not null,
+  customer_id varchar(50),
+  employee_id integer,
+  order_date date,
+  ship_city varchar(50),
+  foreign key (customer_id) references customers (customer_id)
+  match simple on update no action on delete no action,
+  foreign key (employee_id) references employee (employee_id)
+  match simple on update no action on delete no action
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,57 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2  # Импорт библиотеки для работы с PostgreSQL
+import csv  # Импорт библиотеки для работы с CSV файлами
+
+
+def import_data_to_database():
+    """
+    Импортирует данные из CSV файлов в таблицы базы данных PostgreSQL.
+
+    Предполагается, что существуют следующие таблицы:
+    - customers (customer_id, company_name, contact_name)
+    - employee (employee_id, first_name, last_name, title, birth_date, notes)
+    - orders (order_id, customer_id, employee_id, order_date, ship_city)
+
+    CSV файлы ожидаются в директории 'north_data'.
+
+    Возвращает:
+        None
+    """
+    conn = psycopg2.connect(
+        host="localhost",  # Хост базы данных
+        database="postgres",  # Имя базы данных
+    )
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                with open('north_data/customers_data.csv') as file:
+                    reader = csv.DictReader(file)  # Создание читателя CSV файла
+                    for row in reader:
+                        # Вставка данных из CSV файла в таблицу customers
+                        cur.execute(
+                            "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                            (row['customer_id'], row['company_name'], row['contact_name']))
+                with open('north_data/employees_data.csv') as file:
+                    reader = csv.DictReader(file)  # Создание читателя CSV файла
+                    for row in reader:
+                        # Вставка данных из CSV файла в таблицу employee
+                        cur.execute(
+                            "INSERT INTO employee (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s"
+                            ", %s, %s, %s, %s, %s)",
+                            (row['employee_id'], row['first_name'], row['last_name'], row['title'], row['birth_date'],
+                             row['notes']))
+                with open('north_data/orders_data.csv') as file:
+                    reader = csv.DictReader(file)  # Создание читателя CSV файла
+                    for row in reader:
+                        # Вставка данных из CSV файла в таблицу orders
+                        cur.execute(
+                            "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s"
+                            ", %s, %s, %s, %s)",
+                            (row['order_id'], row['customer_id'], row['employee_id'], row['order_date'],
+                             row['ship_city']))
+
+    finally:
+        conn.close()
+
+
+# Вызов функции для импорта данных в базу данных
+import_data_to_database()
